f25774f6bdb4098a78a6c0362d323576f13444b9
鈴木聡士 <suzukisatoshi@suzukisatoshinoMacBook-Pro.local>
2019-06-23 09:31:41 +0900

    onMouseDown 時に条件が抜けてたので追加 (※console.log 沢山でます) #1

--- src/Canvas/index.tsx
+++ src/Canvas/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 
 const style = {
-  border: '1px solid gray',
-  backgroundColor: 'white',
+  root: {
+    // ※className　使わないと余計な div が増えてスタイリングできないので以下調整  
+    // display: 'flex',
+    // justifyContent: 'center',
+    margin: '2rem',
+    width: '500px',
+    height: '500px',
+    border: '1px solid gray',
+    borderRadius: '10px',
+  },
+  canvas: {},
 };
 
 interface Props extends React.Props<any> {}
@@ -11,9 +20,9 @@ interface State {
   drawing: boolean;
 }
 
-interface Canvas {
-  getContext(contextId: '2d'): CanvasRenderingContext2D;
-}
+// interface Canvas {
+//   getContext(contextId: '2d'): CanvasRenderingContext2D;
+// }
 
 class Canvas extends React.Component<Props, State> {
   constructor(props: any) {
@@ -30,23 +39,29 @@ class Canvas extends React.Component<Props, State> {
     this.setState({ drawing: true });
     const ctx = this.getContext();
     ctx.moveTo(x, y);
+    console.log("startDrawing: this.state.drawing", this.state.drawing);
+    console.log("startDrawing: ctx", ctx);
   }
 
   draw(x: number, y: number) {
-    if (this.state.drawing) {
+    if (this.state.drawing === false) {
       return;
     }
     const ctx = this.getContext();
     ctx.lineTo(x, y);
     ctx.stroke();
+    console.log("draw: this.state.drawing", this.state.drawing);
+    console.log("draw: ctx", ctx);
   }
 
   endDrawing() {
     this.setState({ drawing: false });
+    console.log("endDrawing: this.state.drawing", this.state.drawing);
   }
 
   render() {
     return (
+      <div style={style.root}>
       <canvas
         ref="canvas"
         width="500px"
@@ -55,8 +70,9 @@ class Canvas extends React.Component<Props, State> {
         onMouseUp={() => this.endDrawing()}
         onMouseLeave={() => this.endDrawing()}
         onMouseMove={e => this.draw(e.nativeEvent.offsetX, e.nativeEvent.offsetY)}
-        style={style}
+        style={style.canvas}
       />
+      </div>
     );
   }
 }
